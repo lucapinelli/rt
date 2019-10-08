1d29a5950a1dacf042098ea4995f3620305d1037
Luca Pinelli <pinelli.luca@gmail.com>
2019-10-08 20:23:03 +0200

    feature/option-development

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::env;
 use std::error::Error;
 use std::fs;
@@ -16,13 +17,17 @@ Params:
                                relative -- show the relative path of the item
                                absolute -- show the absolute path of the item
     hidden=BOOLEAN,h=BOOLEAN   if true we will show hidden item (dafault: false)
+    dev=BOOLEAN,d=BOOLEAN      if true standard development folders and files are discarded
+                               (.git, target, node_modules, build, bin)
 ";
 
+#[derive(Debug, Clone)]
 struct Arguments {
     path: String,
     levels: u32,
     style: String,
     hidden: bool,
+    development: bool,
 }
 impl Arguments {
     fn new(args: &Vec<String>) -> Result<Arguments, Box<dyn Error>> {
@@ -30,6 +35,7 @@ impl Arguments {
         let mut levels = 0;
         let mut style = String::from("");
         let mut hidden = false;
+        let mut development = false;
         if args.len() < 1 {
             eprintln!("{}", HELP);
             process::exit(1);
@@ -59,6 +65,15 @@ impl Arguments {
                 hidden = value
                     .parse()
                     .expect("\"hidden\" must be \"true\" or \"false\".");
+            } else if arg.starts_with("development=") || arg.starts_with("d=") {
+                let value = if arg.starts_with("d=") {
+                    &arg[2..]
+                } else {
+                    &arg[12..]
+                };
+                development = value
+                    .parse()
+                    .expect("\"development\" must be \"true\" or \"false\".");
             } else if path.is_empty() {
                 path = arg.to_string();
             } else {
@@ -71,70 +86,93 @@ impl Arguments {
             levels,
             style,
             hidden,
+            development,
         })
     }
 }
 
-fn get_name(path: &Path) -> Result<(String), Box<dyn Error>> {
-    match path.file_name() {
-        Some(name) => Ok(name.to_str().ok_or("")?.to_string()),
-        None => match path.canonicalize()?.file_name() {
-            Some(name) => Ok(name.to_str().ok_or("")?.to_string()),
-            None => Ok(path.canonicalize()?.to_str().ok_or("")?.to_string()),
-        },
-    }
+#[derive(Debug)]
+struct Tree {
+    options: Arguments,
+    dev_excludes: HashSet<String>, //HashSet::from_iter
 }
 
-fn visit_path(
-    path: &Path,
-    level: u32,
-    max_level: u32,
-    style: &String,
-    hidden: bool,
-) -> Result<(), Box<dyn Error>> {
-    if !hidden && get_name(&path)?.starts_with('.') {
-        return Ok(());
-    }
-    print_item(&path, level, style)?;
-    if max_level != 0 && level >= max_level {
-        return Ok(());
-    }
-    if path.is_dir() {
-        for entry in fs::read_dir(path)? {
-            visit_path(&entry?.path(), level + 1, max_level, style, hidden)?;
+impl Tree {
+    fn new(options: &Arguments) -> Tree {
+        let mut dev_excludes = HashSet::new();
+        dev_excludes.insert(".git".to_string());
+        dev_excludes.insert("target".to_string());
+        dev_excludes.insert("node_modules".to_string());
+        dev_excludes.insert("build".to_string());
+        dev_excludes.insert("bin".to_string());
+        Tree {
+            options: options.clone(),
+            dev_excludes,
         }
     }
-    Ok(())
-}
 
-fn print_item(path: &Path, level: u32, style: &String) -> Result<(), Box<dyn Error>> {
-    let item = if style == "absolute" {
-        path.canonicalize()?.to_str().ok_or("--")?.to_string()
-    } else if style == "relative" {
-        path.to_str().ok_or("--")?.to_string()
-    } else {
-        let mut pad: String = format!("{: >level$}* ", "", level = (level * 2) as usize);
-        pad.push_str(&get_name(path)?);
-        pad
-    };
-    println!("{}", &item);
-    Ok(())
+    fn get_name(&self, path: &Path) -> Result<(String), Box<dyn Error>> {
+        match path.file_name() {
+            Some(name) => Ok(name.to_str().ok_or("")?.to_string()),
+            None => match path.canonicalize()?.file_name() {
+                Some(name) => Ok(name.to_str().ok_or("")?.to_string()),
+                None => Ok(path.canonicalize()?.to_str().ok_or("")?.to_string()),
+            },
+        }
+    }
+
+    fn visit_path(&self, path: &Path, level: u32) -> Result<(), Box<dyn Error>> {
+        let Arguments {
+            path: _path,
+            levels: max_level,
+            style,
+            hidden,
+            development,
+        } = &self.options;
+
+        let name = self.get_name(&path)?;
+        if !hidden && name.starts_with('.') {
+            return Ok(());
+        }
+        if *development && self.dev_excludes.contains(&name) {
+            return Ok(());
+        }
+        self.print_item(&path, level, &style)?;
+        if *max_level != 0 as u32 && level >= *max_level {
+            return Ok(());
+        }
+        if path.is_dir() {
+            for entry in fs::read_dir(path)? {
+                self.visit_path(&entry?.path(), level + 1)?;
+            }
+        }
+        Ok(())
+    }
+
+    fn print_item(&self, path: &Path, level: u32, style: &String) -> Result<(), Box<dyn Error>> {
+        let item = if style == "absolute" {
+            path.canonicalize()?.to_str().ok_or("--")?.to_string()
+        } else if style == "relative" {
+            path.to_str().ok_or("--")?.to_string()
+        } else {
+            let mut pad: String = format!("{: >level$}* ", "", level = (level * 2) as usize);
+            pad.push_str(&&self.get_name(path)?);
+            pad
+        };
+        println!("{}", &item);
+        Ok(())
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let Arguments {
-        path: path_str,
-        levels,
-        style,
-        hidden,
-    } = Arguments::new(&env::args().skip(1).collect())?;
-    let path = Path::new(&path_str);
+    let arguments = Arguments::new(&env::args().skip(1).collect())?;
+    let path = Path::new(&arguments.path);
     if !path.exists() {
         eprintln!("\nERROR: the specified path does not reference to any file or directory.\n");
         eprintln!("{}", HELP);
         process::exit(1);
     }
-    visit_path(&path, 0, levels, &style, hidden)?;
-
+    let tree = Tree::new(&arguments);
+    tree.visit_path(path, 0)?;
     Ok(())
 }
